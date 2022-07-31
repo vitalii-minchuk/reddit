7938bf43f2e1212f326ad770a8decc914ed16c24
vitalii-minchuk <mcmin4doutsch@gmail.com>
2022-07-31 22:55:33 +0300

    firestore, functions

--- src/components/Modal/CreateCommunity/index.tsx
+++ src/components/Modal/CreateCommunity/index.tsx
@@ -18,9 +18,17 @@ import {
   Stack,
   Text,
 } from "@chakra-ui/react";
-import { BsFillEyeFill, BsFillPersonFill } from "react-icons/bs"
-import { HiLockClosed } from "react-icons/hi"
+import { BsFillEyeFill, BsFillPersonFill } from "react-icons/bs";
+import { HiLockClosed } from "react-icons/hi";
+import { doc, getDoc, serverTimestamp, setDoc } from "firebase/firestore";
+import { auth, firestore } from "../../../firebase/clientApp";
+import { useAuthState } from "react-firebase-hooks/auth";
 
+enum ComType {
+  public = "public",
+  restricted = "restricted",
+  private = "private",
+}
 
 interface ICreateCommunityModalProps {
   open: boolean;
@@ -31,16 +39,19 @@ const CreateCommunityModal: FC<ICreateCommunityModalProps> = ({
   open,
   handleClose,
 }) => {
+  const [user] = useAuthState(auth);
   const [communityName, setCommunityName] = useState("");
-  const [communityType, setCommunityType] = useState("Public");
+  const [communityType, setCommunityType] = useState(ComType.public);
   const [charsRemaining, setCharsRemaining] = useState(21);
+  const [nameError, setNameError] = useState("");
+  const [loading, setLoading] = useState(false)
 
   useEffect(() => {
     setCommunityName("");
     setCharsRemaining(21);
   }, [open]);
 
-  const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
+  const handleNameChange = (event: ChangeEvent<HTMLInputElement>): void => {
     if (event.target.value.length > 21) {
       return;
     }
@@ -48,9 +59,43 @@ const CreateCommunityModal: FC<ICreateCommunityModalProps> = ({
     setCharsRemaining(21 - event.target.value.length);
   };
 
+  const handleTypeChange = (nextValue: string): void => {
+    setCommunityType(nextValue as ComType);
+  };
+
+  const handleCreateCommunity = async () => {
+    if (nameError) setNameError("");
+    const format = /[ `!@#$%^&*()+\-=\[\]{};':"\\|,.<>\/?~]/;
+    if (format.test(communityName) || communityName.length < 3) {
+      return setNameError(
+        "Community names must be between 3–21 characters, and can only contain letters, numbers, or underscores."
+      );
+    }
+    setLoading(true)
+    try {
+      const communityDocRef = doc(firestore, "communities", communityName);
+      const communityDoc = await getDoc(communityDocRef);
+      if (communityDoc.exists()) {
+        throw new Error(`Sorry, /r${communityName} is taken. Try another.`)
+      }
+      await setDoc(communityDocRef, {
+        creatorId: user?.uid,
+        createdAt: serverTimestamp(),
+        numberOfMembers: 1,
+        privacyType: communityType,
+      });
+    } catch (error: any) {
+      console.log(error.message)
+      setNameError(error.message)
+
+    }
+
+    setLoading(false)
+  };
+  
   return (
     <>
-      <Modal isOpen={open} onClose={handleClose} size="md">
+      <Modal isOpen={open} onClose={handleClose} size="lg">
         <ModalOverlay />
         <ModalContent mx={2}>
           <ModalHeader>Create a community</ModalHeader>
@@ -74,7 +119,7 @@ const CreateCommunityModal: FC<ICreateCommunityModalProps> = ({
               </Text>
               <Input
                 position="relative"
-                onChange={handleChange}
+                onChange={handleNameChange}
                 value={communityName}
                 size="sm"
                 pl="22px"
@@ -89,43 +134,80 @@ const CreateCommunityModal: FC<ICreateCommunityModalProps> = ({
               >
                 {charsRemaining} Characters remaining
               </Text>
+              <Text fontSize="9pt" color="red.500">
+                {nameError}
+              </Text>
               <Box>
                 <Text fontSize={15} fontWeight={600} my={4}>
                   Community Type
                 </Text>
-                <RadioGroup onChange={setCommunityType} value={communityType}>
-                  <Flex direction="column" gap={4} >
-                    <Radio value="Public">
+                <RadioGroup onChange={handleTypeChange} value={communityType}>
+                  <Flex direction="column" gap={4}>
+                    <Radio value={ComType.public}>
                       <Flex align="center" gap={2}>
                         <Icon color="gray.500" as={BsFillPersonFill} />
                         <Text fontSize="10pt">Public</Text>
-                        <Text alignSelf="start" fontSize="8pt" color="gray.500">
+                        <Text
+                          pt="2px"
+                          alignSelf="start"
+                          fontSize="8pt"
+                          color="gray.500"
+                        >
                           Anyone can view, post and comment to this community
                         </Text>
                       </Flex>
                     </Radio>
-                    <Divider borderColor={communityType === "Public" ? "blue.500" : "inherit"} />
-                    <Radio value="Restricted">
+                    <Divider
+                      borderColor={
+                        communityType === ComType.public
+                          ? "blue.500"
+                          : "inherit"
+                      }
+                    />
+                    <Radio value={ComType.restricted}>
                       <Flex align="center" gap={2}>
                         <Icon color="gray.500" as={BsFillEyeFill} />
                         <Text fontSize="10pt">Restricted</Text>
-                        <Text alignSelf="start" fontSize="8pt" color="gray.500">
+                        <Text
+                          pt="2px"
+                          alignSelf="start"
+                          fontSize="8pt"
+                          color="gray.500"
+                        >
                           Anyone can view this community, but only approved
                           users can post
                         </Text>
                       </Flex>
                     </Radio>
-                    <Divider borderColor={communityType === "Restricted" ? "blue.500" : "inherit"} />
-                    <Radio value="Private">
+                    <Divider
+                      borderColor={
+                        communityType === ComType.restricted
+                          ? "blue.500"
+                          : "inherit"
+                      }
+                    />
+                    <Radio value={ComType.private}>
                       <Flex align="center" gap={2}>
                         <Icon color="gray.500" as={HiLockClosed} />
                         <Text fontSize="10pt">Private</Text>
-                        <Text alignSelf="start" fontSize="8pt" color="gray.500">
-                          Only approved users can view and submit to this community
+                        <Text
+                          pt="2px"
+                          alignSelf="start"
+                          fontSize="8pt"
+                          color="gray.500"
+                        >
+                          Only approved users can view and submit to this
+                          community
                         </Text>
                       </Flex>
                     </Radio>
-                    <Divider borderColor={communityType === "Private" ? "blue.500" : "inherit"} />
+                    <Divider
+                      borderColor={
+                        communityType === ComType.private
+                          ? "blue.500"
+                          : "inherit"
+                      }
+                    />
                   </Flex>
                 </RadioGroup>
               </Box>
@@ -135,7 +217,9 @@ const CreateCommunityModal: FC<ICreateCommunityModalProps> = ({
             <Button variant="outline" h="30px" mr={3} onClick={handleClose}>
               Cancel
             </Button>
-            <Button  h="30px" onClick={() => {}}>Create Community</Button>
+            <Button h="30px" isLoading={loading} onClick={handleCreateCommunity}>
+              Create Community
+            </Button>
           </ModalFooter>
         </ModalContent>
       </Modal>
